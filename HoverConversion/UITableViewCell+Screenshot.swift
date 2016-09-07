8b1ebbf1ad401a7d2f2236ae57db2d8aa7289782
szk-atmosphere <s1180183@gmail.com>
2016-09-08 04:13:57 +0900

    add tableviewcell screenshot

---
+++ HoverConversion/UITableViewCell+Screenshot.swift
@@ -0,0 +1,19 @@
+//
+//  UITableViewCell+Screenshot.swift
+//  HoverConversion
+//
+//  Created by Taiki Suzuki on 2016/09/08.
+//  Copyright © 2016年 marty-suzuki. All rights reserved.
+//
+
+import UIKit
+
+extension UITableViewCell {
+    func screenshot(scale: CGFloat = UIScreen.mainScreen().scale) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(bounds.size, false, scale)
+        drawViewHierarchyInRect(self.bounds, afterScreenUpdates: true)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image
+    }
+}
