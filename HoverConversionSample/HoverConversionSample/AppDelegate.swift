c6087da8ea2efbb0d8f6f579981e441a1346ab37
marty-suzuki <s1180183@gmail.com>
2016-09-21 10:11:54 +0900

    add TouchVisualizer again

--- HoverConversionSample/HoverConversionSample/AppDelegate.swift
+++ HoverConversionSample/HoverConversionSample/AppDelegate.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Fabric
 import TwitterKit
-//import TouchVisualizer
+import TouchVisualizer
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         Fabric.with([Twitter.self])
-        //Visualizer.start()
+        Visualizer.start()
         return true
     }
 
@@ -31,22 +31,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-        //Visualizer.stop()
+        Visualizer.stop()
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        Visualizer.start()
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        Visualizer.start()
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-        //Visualizer.stop()
+        Visualizer.stop()
     }
-
-
 }
-
